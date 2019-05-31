fd5de5f1605e9295d84da46aa938e06224302316
Abdelrahman Ahmed <37315602+abdelrahman84@users.noreply.github.com>
2019-05-31 23:21:32 +0200

    Update App.js

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { Platform, StyleSheet, Text, View, TouchableOpacity, Linking, Button } from 'react-native';
 import { createStackNavigator } from 'react-navigation';
 import ProductScreen from './components/ProductScreen';
 import ProductDetailScreen from './components/ProductDetailScreen';
 import AddProductScreen from './components/AddProductScreen';
 import EditProductScreen from './components/EditProductScreen';
+
 
 const RootStack = createStackNavigator(
   {
@@ -31,12 +32,22 @@ const RootStack = createStackNavigator(
 
 export default class App extends React.Component { 
  
-  
+  login(){
+    Linking.openURL('https://api.instagram.com/oauth/authorize/?client_id=963a89c1b7384592abd2a17bfa19913b&redirect_uri=Product&response_type=token');
+  }
 
 
   render() {
-    return <RootStack />;
-     
+    return [
+      
+  <RootStack />,
+  <Button
+  onPress={this.login}
+  title="Login with Instagram"
+  color="#841584"
+  accessibilityLabel="Learn more about this purple button"/>
+
+    ]
   }
 }
 
